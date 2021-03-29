9ca3da50a51d6c89adf4bdc8889a43081ce020e9
Julio Fuentes <juliojavif@gmail.com>
2021-03-29 10:13:06 -0500

    Login y resgistro funcionando correctamente

--- app/src/main/java/com/example/androidtaller1/RegisterActivity.java
+++ app/src/main/java/com/example/androidtaller1/RegisterActivity.java
@@ -2,33 +2,70 @@ package com.example.androidtaller1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
 
 public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner spiner;
     String text;
 
+    Button guardar;
+    EditText txtNombre, txtApellido, txtCorreo, txtContrasena;
+    DAOUsuario dao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        spiner=findViewById(R.id.spinersexo);
+
+        spiner=(Spinner) findViewById(R.id.spinersexo);
         ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this, R.array.opsexo, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spiner.setAdapter(adapter);
 
         spiner.setOnItemSelectedListener(this);
+        guardar = findViewById(R.id.btnguardar);
+        txtNombre = findViewById(R.id.edtnombre);
+        txtApellido = findViewById(R.id.edtapellido);
+        txtCorreo = findViewById(R.id.edtcorreo);
+        txtContrasena = findViewById(R.id.edtpassword);
+
+        dao = new DAOUsuario(this);
+
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Usuario u = new Usuario();
+                u.setNombre(txtNombre.getText().toString());
+                u.setApellido(txtApellido.getText().toString());
+                u.setCorreo(txtCorreo.getText().toString());
+                u.setContrasena(txtContrasena.getText().toString());
+                u.setSexo(spiner.getSelectedItem().toString());
+                if (u.isNull()){
+                    Toast.makeText(RegisterActivity.this, "Error: campos vacíos", Toast.LENGTH_LONG).show();
+                }else if (dao.insertUsuario(u)){
+                    Toast.makeText(RegisterActivity.this, "Registro exitoso", Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }else{
+                    Toast.makeText(RegisterActivity.this, "Usuario ya registrado", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         text=parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+        //Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
     }
 
     @Override
