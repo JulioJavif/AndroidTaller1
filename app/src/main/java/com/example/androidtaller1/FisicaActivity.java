9e5f73cf024efc452dbf932f05974c862539c6b9
Daniel Fontalvo <danielfon1999@gmail.com>
2021-03-30 15:05:04 -0500

    Vista de fisica/velocidad

---
+++ app/src/main/java/com/example/androidtaller1/FisicaActivity.java
@@ -0,0 +1,40 @@
+package com.example.androidtaller1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FisicaActivity extends AppCompatActivity implements View.OnClickListener {
+    Button velocidad,fuerza,voltaje;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fisica);
+        voltaje=findViewById(R.id.btnVoltaje);
+        fuerza=findViewById(R.id.btnFuerza);
+        velocidad=findViewById(R.id.btnVelocidad);
+        velocidad.setOnClickListener(this);
+        fuerza.setOnClickListener(this);
+        voltaje.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()){
+            case R.id.btnVelocidad:
+                i= new Intent(getApplicationContext(),DistanciaActivity.class);
+                startActivity(i);
+                break;
+            case R.id.btnFuerza:
+                i= new Intent(getApplicationContext(),FuerzaActivity.class);
+                startActivity(i);
+                break;
+        }
+    }
+}
